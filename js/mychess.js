e1733fc4c00702f0987fa362881ec50f1c8785ab
asidirop <asidirop@gmail.com>
2020-12-08 15:08:23 +0200

    chess4

--- js/mychess.js
+++ js/mychess.js
@@ -1,5 +1,6 @@
 var me={};
 var game_status={};
+var board={};
 
 $(function () {
 	draw_empty_board();
@@ -10,6 +11,8 @@ $(function () {
 	$('#do_move').click( do_move);
 	$('#move_div').hide();
 	game_status_update();
+	$('#the_move_src').change( update_moves_selector);
+	$('#do_move2').click( do_move2);
 });
 
 
@@ -32,17 +35,24 @@ function draw_empty_board(p) {
 	t+='</table>';
 	
 	$('#chess_board').html(t);
+	$('.chess_square').click(click_on_piece);
 }
 
 function fill_board() {
-	$.ajax({url: "chess.php/board/", success: fill_board_by_data });
+	$.ajax({url: "chess.php/board/", 
+		headers: {"X-Token": me.token},
+			//dataType: "json",
+			//contentType: 'application/json',
+			//data: JSON.stringify( {token: me.token}),
+			success: fill_board_by_data });
 }
 function reset_board() {
-	$.ajax({url: "chess.php/board/", method: 'POST',  success: fill_board_by_data });
+	$.ajax({url: "chess.php/board/", headers: {"X-Token": me.token}, method: 'POST',  success: fill_board_by_data });
 	$('#move_div').hide();
 	$('#game_initializer').show(2000);
 }
 function fill_board_by_data(data) {
+	board=data;
 	for(var i=0;i<data.length;i++) {
 		var o = data[i];
 		var id = '#square_'+ o.x +'_' + o.y;
@@ -65,6 +75,7 @@ function login_to_game() {
 	$.ajax({url: "chess.php/players/"+p_color, 
 			method: 'PUT',
 			dataType: "json",
+			headers: {"X-Token": me.token},
 			contentType: 'application/json',
 			data: JSON.stringify( {username: $('#username').val(), piece_color: p_color}),
 			success: login_result,
@@ -84,15 +95,19 @@ function login_error(data,y,z,c) {
 }
 
 function game_status_update() {
-	$.ajax({url: "chess.php/status/", success: update_status });
+	$.ajax({url: "chess.php/status/", success: update_status,headers: {"X-Token": me.token} });
 }
 
 function update_status(data) {
+	var game_stat_old = game_status;
 	game_status=data[0];
 	update_info();
 	if(game_status.p_turn==me.piece_color &&  me.piece_color!=null) {
 		x=0;
 		// do play
+		if(game_stat_old.p_turn!=me.piece_color) {
+			fill_board();
+		}
 		$('#move_div').show(1000);
 		setTimeout(function() { game_status_update();}, 15000);
 	} else {
@@ -105,6 +120,7 @@ function update_status(data) {
 
 function update_info(){
 	$('#game_info').html("I am Player: "+me.piece_color+", my name is "+me.username +'<br>Token='+me.token+'<br>Game state: '+game_status.status+', '+ game_status.p_turn+' must play now.');
+	
 	
 }
 
@@ -121,11 +137,52 @@ function do_move() {
 			dataType: "json",
 			contentType: 'application/json',
 			data: JSON.stringify( {x: a[2], y: a[3]}),
+			headers: {"X-Token": me.token},
 			success: move_result,
 			error: login_error});
 	
 }
 
 function move_result(data){
+	fill_board_by_data(data);
+	$('#move_div').hide(1000);
+}
+
+function update_moves_selector() {
+	$('.chess_square').removeClass('pmove').removeClass('tomove');
+	var s = $('#the_move_src').val();
+	var a = s.trim().split(/[ ]+/);
+	$('#the_move_dest').html('');
+	if(a.length!=2) {
+		return;
+	}
+	var id = '#square_'+ a[0]+'_'+a[1];
+	$(id).addClass('tomove');
+	for(i=0;i<board.length;i++) {
+		if(board[i].x==a[0] && board[i].y==a[1]) {
+			for(m=0;m<board[i].moves.length;m++) {
+				$('#the_move_dest').append('<option value="'+board[i].moves[m].x+' '+board[i].moves[m].y+'">'+board[i].moves[m].x+' '+board[i].moves[m].y+'</option>');
+				var id = '#square_'+ board[i].moves[m].x +'_' + board[i].moves[m].y;
+				$(id).addClass('pmove');
+			}
+			
+		}
+	}
+}
+
+function do_move2() {
+	$('#the_move').val($('#the_move_src').val() +' ' + $('#the_move_dest').val());
+	do_move();
+	$('.chess_square').removeClass('pmove').removeClass('tomove');
+}
+
+function click_on_piece(e) {
+	var o=e.target;
+	if(o.tagName!='TD') {o=o.parentNode;}
+	if(o.tagName!='TD') {return;}
 	
+	var id=o.id;
+	var a=id.split(/_/);
+	$('#the_move_src').val(a[1]+' ' +a[2]);
+	update_moves_selector();
 }
